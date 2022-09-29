972bde66885087d3488a185a2ce288f9a83c528c
LuisKain <96962494+LuisKain@users.noreply.github.com>
2022-09-29 08:46:49 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        var calculator = new SpecialCalculator();
-        System.out.println(calculator.circleDiameter(5));
     }
 }
